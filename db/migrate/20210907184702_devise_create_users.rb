55d51884c3699ec4931aee018e7d7553f5a851e2
therealjustindude <therealjustindude@gmail.com>
2021-09-07 16:17:02 -0400

    finished installing devise

--- db/migrate/20210907184702_devise_create_users.rb
+++ db/migrate/20210907184702_devise_create_users.rb
@@ -8,8 +8,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
 
        ## Other attributes
-       t.string :first_name, null: false
-       t.string :last_name, null: false 
+       t.string :first_name
+       t.string :last_name
        
       ## Recoverable
       t.string   :reset_password_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# -------------------User One------------------------------------------------
+
+user_one = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'password')
+
+# --------------------User One Expenses--------------------------------------
+
+user_one.expenses.create(name: 'Rent', date_due: '09/07/2021', date_paid: '' , auto_withdrawl: true, bank_account_name: 'Chase Joint', amount_owed: 1200.68)
+user_one.expenses.create(name: 'Netflix', date_due: '09/02/2021', date_paid: '09/01/2021' , auto_withdrawl: true, bank_account_name: 'Justin\'s Chase', amount_owed: 14.98,)
+user_one.expenses.create(name: 'Internet', date_due: '09/14/2021', date_paid: '' , auto_withdrawl: true, bank_account_name: 'Chase Joint', amount_owed: 120.55,)
+
+# --------------------User Two-----------------------------------------------
+
+user_two = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'password')
+
+# --------------------User Two Expenses--------------------------------------
+
+user_two.expenses.create(name: 'Rent', date_due: '09/07/2021', date_paid: '' , auto_withdrawl: true, bank_account_name: 'Chase Joint', amount_owed: 1400.65,)
+user_two.expenses.create(name: 'Netflix', date_due: '09/02/2021', date_paid: '09/01/2021' , auto_withdrawl: true, bank_account_name: 'Justin\'s Chase', amount_owed: 14.98,)
+user_two.expenses.create(name: 'Internet', date_due: '09/14/2021', date_paid: '' , auto_withdrawl: true, bank_account_name: 'Chase Joint', amount_owed: 80.32,)
+
+# ----------------------------------------------------------------------------
